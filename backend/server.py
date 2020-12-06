974957ce3ab12b7762a861115d715d0578b28a0d
earlyr1 <earlyr@yandex.ru>
2020-12-06 14:58:02 +0300

    added api tables

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,12 @@
 from aiohttp import web
 
-from src.urls import echo
+from src.urls import echo, newTableFromFile
 
 app = web.Application()
-app.add_routes([web.get('/api/echo/', echo)])
+app.add_routes([
+                web.get('/api/echo/', echo),
+                web.post('/api/tables', newTableFromFile)
+               ])
 
 if __name__ == '__main__':
     web.run_app(app, port=5000)
--- backend/src/urls.py
+++ backend/src/urls.py
@@ -2,7 +2,11 @@ import typing
 
 from aiohttp import web
 
-def echo(request: web.Request) -> web.Response:
+async def echo(request: web.Request) -> web.Response:
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
     return web.Response(text=text)
+
+async def newTableFromFile(request: web.Request) -> web.Response:
+    print(request)
+    return web.Response(text="kek")
