92cba5913120f0e04302f680895ffd9c1717fe30
bramov <alexey.abramov96@gmail.com>
2020-12-06 15:45:20 +0300

    fixed dragndrop functionalty

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,63 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  /*
-  const button = document.querySelector('button');
-  const canvas = document.querySelector('canvas');
-
-  button.addEventListener('click', () => {
-    button.style.display = 'none';
-    canvas.style.display = 'block';
-    renderCanvas();
-  })
-
-  const renderCanvas = () => {
-    const ctx = canvas.getContext('2d');
-    const myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    })
-  }
-  */
-
 
 
   const file = document.getElementById('fileupload');
 
   file.addEventListener('change', (e) => {
     e.preventDefault();
-    dropBox.submit();
-
+    handleFile();
   })
 
 
@@ -81,10 +29,12 @@ document.addEventListener('DOMContentLoaded', () => {
     dropArea.addEventListener(eventName, unhighlight, false)
   });
 
-  dropArea.addEventListener('drop', dropBox.submit);
+  dropArea.addEventListener('drop', (e) => {
+    handleFile(e.dataTransfer.files);
+  });
+
 
   function highlight(e) {
-    console.log('dragenter');
     dropBox.classList.add('highlight');
     dropArea.classList.add('highlight2');
   }
@@ -97,5 +47,17 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+const handleFile = (file) => {
+  let formData = new FormData();
+  if (file) {
+    formData.append('file', file);
+  }
+  fetch('/api/tables', {
+    method: 'POST',
+    body: formData
+  })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
 
 
